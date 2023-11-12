eb12026e637dfe89e88168abdc651be4181f0c69
Robert Graupner <robert.graupnerr@gmail.com>
2023-11-12 17:25:43 +0100

    add filter option

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -2,28 +2,41 @@ import { useState, useEffect } from 'react';
 import { List } from '../List/List';
 import { Form } from '../Form/Form';
 import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
+import { FilterButton } from '../FilterButton/FilterButton';
 import styles from './Panel.module.css';
+
+// zapisaliśmy adres do API w zmiennej, żeby nie powtarzać go w wielu miejscach
+const url = 'http://localhost:3000/words';
 
 export function Panel() {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [error, setError] = useState(null);
+	const [selectedCategory, setSelectedCategory] = useState(null);
 
 	useEffect(() => {
-		handleLoadData();
-	}, []);
-
-	function handleLoadData() {
-		fetch('http://localhost:3000/words')
+		// jeśli category jest null, to zwracamy pusty string (bez parametru category) i pobieramy wszystkie słowa
+		const params = selectedCategory ? `?category=${selectedCategory}` : '';
+		fetch(`${url}${params}`)
 			.then((res) => res.json())
 			.then((res) => {
 				setData(res);
 				setIsLoading(false);
 			});
-	}
+	}, [selectedCategory]);
+
+	// nie potrzebujemy już tej funkcji, w useEffect mamy zależność od selectedCategory
+	// function handleLoadData() {
+	// 	fetch('http://localhost:3000/words')
+	// 		.then((res) => res.json())
+	// 		.then((res) => {
+	// 			setData(res);
+	// 			setIsLoading(false);
+	// 		});
+	// }
 
 	function handleFormSubmit(formData) {
-		fetch('http://localhost:3000/words', {
+		fetch(url, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -32,12 +45,14 @@ export function Panel() {
 		})
 			.then((res) => res.json())
 			.then((res) => {
-				setData((prevData) => [...prevData, res]);
+				if (!selectedCategory || selectedCategory === res.category) {
+					setData((prevData) => [...prevData, res]);
+				}
 			});
 	}
 
 	function handleDeleteItem(id) {
-		fetch(`http://localhost:3000/words/${id}`, {
+		fetch(`${url}/${id}`, {
 			method: 'DELETE',
 		})
 			.then((res) => {
@@ -58,6 +73,11 @@ export function Panel() {
 			});
 	}
 
+	function handleFilterButtonClick(category) {
+		// ustawiamy wybraną kategorię
+		setSelectedCategory(category);
+	}
+
 	if (isLoading) {
 		return <p>Ładowanie...</p>;
 	}
@@ -67,6 +87,23 @@ export function Panel() {
 			{error && <ErrorMessage>{error}</ErrorMessage>}
 			<section className={styles.section}>
 				<Form onFormSubmit={handleFormSubmit} />
+				<div className={styles.filters}>
+					<FilterButton
+						active={selectedCategory === null}
+						onClick={() => handleFilterButtonClick(null)}>
+						Wszystkie
+					</FilterButton>
+					<FilterButton
+						active={selectedCategory === 'verb'}
+						onClick={() => handleFilterButtonClick('verb')}>
+						Czasowniki
+					</FilterButton>
+					<FilterButton
+						active={selectedCategory === 'noun'}
+						onClick={() => handleFilterButtonClick('noun')}>
+						Rzeczownik
+					</FilterButton>
+				</div>
 				<List data={data} onDeleteItem={handleDeleteItem} />
 			</section>
 		</>
