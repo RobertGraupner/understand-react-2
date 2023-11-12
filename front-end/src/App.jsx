bae28276ad815094d29ca2aedf3a95ab0b944e15
Robert Graupner <robert.graupnerr@gmail.com>
2023-11-12 13:41:40 +0100

    add loadind data from backend

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,22 @@
-import styles from "./App.module.css";
-import { Panel } from "./components/Panel/Panel";
+import styles from './App.module.css';
+import { Panel } from './components/Panel/Panel';
+import { Button } from './components/Button/Button';
+import { useState } from 'react';
 
 function App() {
-    return (
-        <main className={styles.main}>
-            <Panel />
-        </main>
-    );
+	const [isPanelShown, setIsPanelShown] = useState(true);
+
+	return (
+		<main className={styles.main}>
+			<Button
+				onClick={() => {
+					setIsPanelShown(!isPanelShown);
+				}}>
+				{isPanelShown ? 'Schowaj panel' : 'Pokaż panel'}
+			</Button>
+			{isPanelShown && <Panel />}
+		</main>
+	);
 }
 
 export default App;
